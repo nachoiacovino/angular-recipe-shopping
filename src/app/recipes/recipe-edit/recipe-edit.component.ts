729b0feec3ccd63905c987d630f1f96d3e7202e9
Nacho Iacovino <ignacio.iacovino@balio.app>
2020-04-30 12:06:14 +0200

    Add FormArray

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormArray } from "@angular/forms";
+import { RecipesService } from "../recipes.service";
 
 @Component({
   selector: "app-recipe-edit",
@@ -12,20 +13,56 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private recipesService: RecipesService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params.id;
+      this.editMode = params.id != null;
+      this.editRecipe();
+    });
+  }
+
+  initForm() {
     this.recipeForm = new FormGroup({
       name: new FormControl(null),
       imagePath: new FormControl(null),
       description: new FormControl(null),
-    });
-
-    this.route.params.subscribe((params: Params) => {
-      this.id = +params.id;
-      this.editMode = params.id != null;
+      ingredients: new FormArray([]),
     });
   }
 
-  onSubmit() {}
+  editRecipe() {
+    this.initForm();
+    if (this.editMode) {
+      const recipe = this.recipesService.getRecipe(this.id);
+      const { name, imagePath, description, ingredients } = recipe;
+
+      let recipeIngredients = this.recipeForm.controls.ingredients as FormArray;
+      if (recipe.ingredients) {
+        for (let ingredient of ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount),
+            })
+          );
+        }
+      }
+
+      this.recipeForm.patchValue({
+        name,
+        imagePath,
+        description,
+        ingredients: recipeIngredients.controls,
+      });
+    }
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
 }
